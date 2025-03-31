367bcf35a1f8c3e2317beb5b02f0771d3a7b8c86
AnaninAN <ananinpgu@mail.ru>
2025-03-31 20:46:40 +0300

    fix_31032025_3

--- src/lib/logger/logger-category.ts
+++ src/lib/logger/logger-category.ts
@@ -3,19 +3,30 @@ import { createLogger, format, transports } from 'winston';
 export type LogCategory = 'auth' | 'user' | 'counterpartyType' | 'counterparty';
 
 const createCategoryLogger = (category: LogCategory) => {
-  return createLogger({
+  const isDevelopment = process.env.NODE_ENV === 'development';
+
+  const options = {
     format: format.combine(format.timestamp(), format.json()),
     transports: [
+      new transports.Console({
+        format: format.combine(format.colorize(), format.simple()),
+      }),
+    ] as Array<
+      transports.ConsoleTransportInstance | transports.FileTransportInstance
+    >,
+  };
+
+  if (isDevelopment) {
+    options.transports.push(
       new transports.File({
         filename: `logs/${category}.log`,
         maxsize: 5242880,
         maxFiles: 5,
-      }),
-      new transports.Console({
-        format: format.combine(format.colorize(), format.simple()),
-      }),
-    ],
-  });
+      })
+    );
+  }
+
+  return createLogger(options);
 };
 
 export const categoryLoggers: Record<
